1bbddc4ca1bb7c35c488ea8e6cf806c770620431
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-19 12:03:49 +0200

    constructor injection and setter injection are better than field injection. Both setter injection and field injectuses @Autowired but for constructor injection it is optional

--- src/main/java/com/namenets/DependencyInjection/Dev.java
+++ src/main/java/com/namenets/DependencyInjection/Dev.java
@@ -5,8 +5,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
-    @Autowired
+//    @Autowired // field injection
     private Laptop laptop;
+
+    // constructor injection
+    @Autowired
+    public Dev(Laptop laptop){
+        System.out.println("It is a constructor injection. We have omitted Autowired annotation because it was a field injection ");
+        this.laptop = laptop;
+    }
+    @Autowired
+    public void setLaptop(Laptop laptop){
+        System.out.println("Constructor injection and Setter injection are better than field injection");
+        this.laptop = laptop;
+    }
     public void build(){
         laptop.compile();
         System.out.println("It is a standalone class or object inside IoC Container managed by Spring.");
