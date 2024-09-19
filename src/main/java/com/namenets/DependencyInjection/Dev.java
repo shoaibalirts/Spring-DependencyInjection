50be15dd1f97a06c614cc01f3266db0ddfd1ecbb
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-19 11:15:19 +0200

    added some more statements

--- src/main/java/com/namenets/DependencyInjection/Dev.java
+++ src/main/java/com/namenets/DependencyInjection/Dev.java
@@ -5,6 +5,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class Dev {
     public void build(){
-        System.out.println("We created a object inside IoC Container");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("It is a standalone class or object inside IoC Container managed by Spring.");
+        System.out.println("It is a standalone object because we have defined the annotation as @Component");
+        System.out.println("Now it is injectable everywhere. It is just like a service");
+        System.out.println("This object can only be injectable via ApplicationContext that has a method getBean");
     }
 }
